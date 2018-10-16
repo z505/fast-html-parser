a7df1b80e6ba19cc9c26c8510efa9d53d6f1b14a
z505 <github@z505.com>
2018-10-15 20:36:03 -0600

    simple demo

--- demo-console/project1.lpr
+++ demo-console/project1.lpr
@@ -1,3 +1,7 @@
+{ Demo for fast html parser
+
+  Try arrays }
+
 program project1;
 
 {$mode objfpc}{$H+}
@@ -6,7 +10,55 @@ uses
   fasthtmlparser,
   sysutils;
 
-begin
+const
+  HTMString1 = '<html><head></head><p>some text</p> <b>text in bold tag</b> more text <p><b>more bold</b></p> and the end of html</html>';
+var
+  InBold: boolean = false;
 
+procedure msg(s: string);
+begin writeln(s);
+end;
+
+procedure msg(s1,s2: string);
+begin writeln(s1,s2);
+end;
+
+//  TOnFoundTagP = procedure(NoCaseTag, ActualTag: string);
+//  TOnFoundTextP = procedure(Text: string);
+
+procedure Ex1OnTag(NoCaseTag, ActualTag: string);
+begin
+  if NoCaseTag = '<B>' then begin
+    InBold := true;
+    msg('bold start');
+  end;
+
+  if NoCaseTag = '</B>' then begin
+    InBold := false;
+    msg('bold end');
+  end;
+end;
+
+procedure Ex1OnText(Text: string);
+begin
+  if InBold then begin
+    msg('Text tag: ', Text);
+  end;
+end;
+
+procedure Example1;
+var
+  hp: THTMLParser;
+begin
+  hp := THTMLParser.create(HTMString1);
+  hp.OnFoundTagP := @Ex1OnTag;
+  hp.OnFoundTextP := @Ex1OnText;
+  hp.Exec;
+  hp.free; hp := nil;
+end;
+
+begin
+  Example1;
+  ReadLn; // pause program on exit for <enter> to quit
 end.
 
--- fasthtmlparser.pas
+++ fasthtmlparser.pas
@@ -1,55 +1,17 @@
 {%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
                     FastHTMLParser unit to parse HTML
                   (disect html into its tags and text.)
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+ Original AUTHOR       : James Azarja, http://www.jazarsoft.com/
 
-
-
- Original AUTHOR       : James Azarja
-                         http://www.jazarsoft.com/
-
- CONTRIBUTORS : L505
-                http://z505.com
-
-                YourName Here...
+ CONTRIBUTORS : L505, http://z505.com
 
  NOTES: Modified by Lars to work with both freepascal and delphi, and contains
         more features
 
-
  LEGAL        : Copyright (C) 2004 Jazarsoft, All Rights Reserved.
                 Modified 2005 Lars (L505)
-
---------------------------------------------------------------------------------
-
-  - Modified for use as a pure command line unit (no dialogs) for freepascal.
-  - Also added UPPERCASE tags so that when you check for <font> it returns all
-    tags like <FONT> and <FoNt> and <font>
-
- Use it for what reasons:
-    -make your own web browsers,
-    -make your own text copies of web pages for caching purposes
-    -Grab content from websites -without- using regular expressions
-    -Seems to be MUCH MUCH FASTER than regular expressions, as it is after all
-     a true parser
-    -convert website tables into spreadsheets (parse <TD> and <TR>, turn in to
-     CSV or similar)
-    -convert websites into text files (parse all text, and tags <BR> <P> )
-    -convert website tables into CSV/Database (<parse <TD> and <TR>)
-    -find certain info from a web page.. i.e. all the bold text or hyperlinks in
-     a page.
-    -Parse websites remotely from a CGI app using something like Sockets or
-     Synapse and SynWrap to first get the HTML site. This would allow you to
-     dynamically parse info from websites and display data on your site in real
-     time.
-    -HTML editor.. WYSIWYG or a partial WYSIWYG editor. Ambitious, but possible.
-    -HTML property editor. Not completely wysiwyg but ability to edit proprties
-     of tags. Work would need to be done to parse each property in a tag.
-
-
 --------------------------------------------------------------------------------
  LICENSE/TERMS
 --------------------------------------------------------------------------------
@@ -67,37 +29,6 @@
  of information, or any other loss), whether or not advised of the
  possibility of damage, and on any theory of liability, arising out of
  or in connection with the use or inability to use this software. 
-
-
---------------------------------------------------------------------------------
- HISTORY:
---------------------------------------------------------------------------------
-
- 0.1     -  James:
-             Initial Development
-             mostly based on Peter Irlam works & ideas
-
- 0.2     -  James:
-             Some minor bug has fixed
-
- 0.3     -  James:
-             Some jsHTMLUtil function bug has been fixed
-
- 0.4     -  James:
-             jsHTMLUtil Tag Attributes bug has been fixed
-             thanks to Dmitry [mail@vader.ru]
-
- 0.4L.1a -  L505:
-             Made unit work with freepascal, added UPCASE (case insensitive)
-             exec function
-
- 0.4L.1b -  L505:
-             Changed case insensitive version to a new class instead of
-             the old ExecUpcase
-
-                                                                                                                                                          //
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 }
 
 {$IFDEF FPC}{$MODE DELPHI}{$H+}{$ENDIF}
